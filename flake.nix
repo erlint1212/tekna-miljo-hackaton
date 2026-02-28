c199f6f35a5efd9b680f22cea71d349607fd48d6
erlint1212 <e.t.nordtvedt@gmail.com>
2026-02-28 11:21:30 +0100

    Inital

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  description = "Kotlin Android App Development Environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {
+            allowUnfree = true;
+            android_sdk.accept_license = true;
+          };
+        };
+
+        # Configure the Android SDK packages we need
+        androidEnv = pkgs.androidenv.composeAndroidPackages {
+          cmdLineToolsVersion = "8.0";
+          toolsVersion = "26.1.1";
+          platformToolsVersion = "34.0.4";
+          buildToolsVersions = [ "34.0.0" ];
+          platformVersions = [ "34" ];
+          abiVersions = [ "x86_64" ];
+          includeEmulator = true;
+          includeSystemImages = false;
+          includeNDK = false;
+        };
+        
+        androidSdk = androidEnv.androidsdk;
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            androidSdk
+            jdk17
+            gradle
+            kotlin
+          ];
+
+          # Essential environment variables for Android development
+          ANDROID_HOME = "${androidSdk}/libexec/android-sdk";
+          ANDROID_SDK_ROOT = "${androidSdk}/libexec/android-sdk";
+          JAVA_HOME = pkgs.jdk17.home;
+          
+          # Fix for AAPT2 errors in Nix environments
+          GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${androidSdk}/libexec/android-sdk/build-tools/34.0.0/aapt2";
+        };
+      }
+    );
+}
